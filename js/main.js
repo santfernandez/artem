81789bbd3f4d8d6576014d9f2e2b38427ca91d94
santfernandez <64820295+santfernandez@users.noreply.github.com>
2021-01-19 10:31:43 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,37 @@
 /**/
 
 const justChecking = (event) => {
-  const h3Content = event.currentTarget.querySelector('.h3').innerText;
-  const h3 = document.createElement('h3')
-  const pContent = event.currentTarget.querySelectorAll('.p');
-  const pArray = [...pContent]
-  console.log(pArray)
-  const p = document.createElement('p')
-  let text = '';
-  const pForEach = () => {
-    pArray.forEach(element => text = element.textContent);
-  }
-  console.log(pForEach())
-  console.log(text)
-  const modalContent = document.createElement('section');
-  const modalDialog = document.createElement('div');
-  const modal = document.createElement('div');
-  const modalDiv = document.getElementById('modal')
-
-  
-  h3.textContent = h3Content;
-  p.textContent = text;
-  console.log(p)
-  modalContent.className = 'modal-content';
-  modalDialog.className = 'modal-dialog';
-  modal.className = 'modal is-visible';
-  
- 
-  modalContent.appendChild(h3);
-  modalContent.appendChild(p);
-  modalDialog.appendChild(modalContent);
-  modal.appendChild(modalDialog);
-  modal.onclick = function() {
-    modalDiv.removeChild(modal);
-  }
-
   if (screen.width <= 479) {
-    //console.log(modal)
-    modalDiv.appendChild(modal);
+    const h3Content = event.currentTarget.querySelector('.h3').innerText;
+    const h3 = document.createElement('h3')
+    const pContent = event.currentTarget.querySelectorAll('.p');
+    const pArray = [...pContent]
+    const p = document.createElement('p')
+    let text = '';
+  
+    const modalContent = document.createElement('section');
+    const modalDialog = document.createElement('div');
+    const modal = document.createElement('div');
+    const modalDiv = document.getElementById('modal')
+    h3.textContent = h3Content;
+    
+    modalContent.className = 'modal-content';
+    modalDialog.className = 'modal-dialog';
+    modal.className = 'modal is-visible';
+    modalContent.appendChild(h3);
+    for (let i = 0; i < pArray.length; i++) {
+        const element = pArray[i].cloneNode(true);
+        console.log(element)
+        modalContent.appendChild(element)
+        ;
+      }
+    console.log(pArray)
+    modalDialog.appendChild(modalContent);
+    modal.appendChild(modalDialog);
+    modal.onclick = function() {
+      modalDiv.removeChild(modal);
+    }
+      modalDiv.appendChild(modal);
   }
   else{
     console.log('funciona');
